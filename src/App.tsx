5ecfc40dbc2bf8668910907e8636ee94ccdee255
Gustavo Alexandre <gustavoslyth1@gmail.com>
2023-04-19 15:45:18 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import './App.css'
+
+function App() {
+
+  return (
+<>
+<BrowserRouter>
+<Routes>
+           <Route index/>
+</Routes>
+</BrowserRouter>
+</>
+)
+}
+
+export default App
