0f5598bf171f2244d6ac84537589b35cf873c185
Gustavo Alexandre <gustavoslyth1@gmail.com>
2023-06-02 18:57:34 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://api-site-pessoal-3qiz.onrender.com/api'
 });
 // baseURL: 'https://json-server-648w.onrender.com/'
 export default api;
